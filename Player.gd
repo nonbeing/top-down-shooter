2582f8dba7bd6a8930422ae3890c87c453ae521a
Ambar <anattaspace@gmail.com>
2019-06-02 15:11:27 +0530

    Multiple changes
    - Use look_at() instead of atan2 for player sprite rotation
    - Use animated sprite for player (idle, move, shoot)

--- Player.gd
+++ Player.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 const MOVE_SPEED = 300
 onready var raycast = $RayCast2D
+onready var sprite = get_node("AnimatedSprite")
+
+var anim = "idle"
 
 func _ready():
 	yield(get_tree(), "idle_frame")
@@ -9,7 +12,7 @@ func _ready():
 	
 func _physics_process(delta):
 	var move_vec = Vector2()
-	var rotation_dir = 0
+	
 	if Input.is_action_pressed("move_up"):
 		move_vec.y -= 1
 	if Input.is_action_pressed("move_down"):
@@ -19,21 +22,31 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		move_vec.x += 1
 
-
-#	rotation += rotation_dir * rotation_speed * delta
-#	move_vec = move_and_slide(move_vec)
 	
 	move_vec = move_vec.normalized()
+#	print("DEBUG x:{x}, y:{y}".format({"x":move_vec.x, "y":move_vec.y}))
+	
+	if move_vec == Vector2.ZERO:
+		anim = "idle"
+	else:
+		anim = "move"
+	
 	move_and_collide(move_vec * MOVE_SPEED * delta)
 	
-	var look_vec = get_global_mouse_position() - global_position
-	global_rotation = atan2(look_vec.y, look_vec.x)
+	# turn towards the mouse pointer
+	look_at(get_global_mouse_position())
+	
+# another way to turn towards the mouse pointer: using vector math
+#	var look_vec = get_global_mouse_position() - global_position
+#	global_rotation = atan2(look_vec.y, look_vec.x)
    
 	if Input.is_action_just_pressed("shoot"):
+		anim = "shoot"
 		var coll = raycast.get_collider()
 		if raycast.is_colliding() and coll.has_method("kill_zombie"):
 			coll.kill_zombie()
 			
+	sprite.play(anim)
  
 func kill_player():
 	# player is dead; restart the game
